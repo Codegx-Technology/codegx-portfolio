0f77082e2b22eee8cd7c0dda2128c59bb4beb9d0
peteroluoch <peteroluoch@gmail.com>
2025-05-22 11:58:00 +0300

    Fix sonner toast dependency and add SonnerToaster to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "@/components/ui/toaster";
+import { Toaster as SonnerToaster } from "sonner";
 import { AnimatePresence } from "framer-motion";
 import { Head } from "@/components/head";
 import CodegxLanding from "@/pages/codegx";
@@ -151,6 +152,7 @@ function App() {
         <Head />
         <Router />
         <Toaster />
+        <SonnerToaster position="top-right" />
         <ReactQueryDevtools initialIsOpen={false} />
       </ThemeProvider>
     </QueryClientProvider>
